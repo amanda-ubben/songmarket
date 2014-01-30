3168e35c5283655c8e8d0e4ca84762750bd879bc
Amanda Ubben <ubbenama@msu.edu>
2014-01-29 21:02:24 -0500

    updates

---
+++ HTML/header.php
@@ -0,0 +1,41 @@
+<?php // header.php
+session_start();
+echo"<!DOCTYPE html>\n<html><head><script src='osc.js'></script>";
+include 'functions.php';
+
+$userstr = ' (Guest)';
+
+if (isset($_SESSION['user']))
+{
+	$user		=$_SESSION['user'];
+	$loggedin = TRUE;
+	$userstr = " ($user)";
+}
+else $loggedin = FALSE;
+
+echo "<title> $appname$userstr</title><link rel='stylesheet'" .
+	"href='main2.css' type='text/css' />" .
+	"</head><body><div class='appname'>$appname$userstr</div>";
+
+if($loggedin)
+{
+	echo "<br ><ul class='menu'>" .
+	     "<li><a href='members.php?view=$user'>Home</a></li>" .
+	     "<li><a href='members.php'>Members</a></li>" .
+	     "<li><a href='viewall.php'>View All</a></li>" .
+	     "<li><a href='friends.php'>Friends</a></li>" .
+	     "<li><a href='booklist.php?view=$user'>Your List</a></li>" .
+	     "<li><a href='profile.php'>Your Profile</a></li>" .
+	     "<li><a href='logout.php'>Log Out</a></li>";
+}
+else
+{
+	echo("<br /><ul class='menu'>" .
+	     "<li><a href='index.php'>Home</a></li>" .
+	     "<li><a href='signup.php'>Sign Up</a></li>" .
+	     "<li><a href='login.php'>Log In</a></li>" .
+	     "<li><a href='viewall.php'>View All</a></li>".
+	     "<span class='info'>&#8658; You must be logged in to" .
+	     "view this page.</span><br /><br />");
+}
+?>
