e27962a014ee8d2b7221667492cae2c711b66182
Matt Ao <mattyayoh@gmail.com>
2014-03-31 17:07:16 -0400

    Fixed login redirections

--- login.php
+++ login.php
@@ -1,42 +1,73 @@
-<?php //login.php
-include_once 'header.php';
-echo"<div class='main'><h3>Please enter you details to log in</h3>";
-$error = $user = $pass = "";
+<?PHP
+require_once("./include/membersite_config.php");
 
-if (isset($_POST['user']))
+if(isset($_POST['submitted']))
 {
-	$user = sanitizeString($_POST['user']);
-	$pass = sanitizeString($_POST['pass']);
-
-	if ($user == "" || $pass == "")
-	{
-		$error = "Not all fields were entered<br />";
-	}
-	else
-	{
-		$query = "SELECT user,pass FROM members WHERE user = '$user' AND pass = '$pass'";
-
-		if (mysql_num_rows(queryMysql($query)) == 0)
-		{
-			$error = "<span class='error'>USername/Password invalid</span><br /><br />";
-		}
-		else
-		{
-			$_SESSION['user'] = $user;
-			$_SESSION['pass'] = $pass;
-			die("You are now logged in. Please <a href='members.php?view=$user'>" . "click here</a> to continue.<br /><br />");
-		}
-	}
+   if($fgmembersite->Login())
+   {
+        $fgmembersite->RedirectToURL("index.php");
+   }
 }
 
-echo <<<_END
-<form method='post' action='login.php'>$error
-<span class='fieldname'>Username</span><input type='text' maxlength='16' name='user' value='$user' /><br />
-<span class='fieldname'>Password</span><input type='text' maxlength='16' name='pass' value='$pass' /><br />
-_END;
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Login</title>
+      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<body>
 
-<br />
-<span class='fieldname'>&nbsp;</span>
-<input type='submit' value='Login' />
-</form><br /></div></body></html>
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='login' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Login</legend>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='username' >UserName*:</label><br/>
+    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
+    <span id='login_username_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='password' >Password*:</label><br/>
+    <input type='password' name='password' id='password' maxlength="50" /><br/>
+    <span id='login_password_errorloc' class='error'></span>
+</div>
+
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+<div class='short_explanation'><a href='reset-pwd-req.php'>Forgot Password?</a></div>
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+
+    var frmvalidator  = new Validator("login");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+
+    frmvalidator.addValidation("username","req","Please provide your username");
+
+    frmvalidator.addValidation("password","req","Please provide the password");
+
+// ]]>
+</script>
+</div>
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
+</body>
+</html>
--- logout.php
+++ logout.php
@@ -1,16 +1,24 @@
-<?php //logout.php
+<?PHP
+require_once("./include/membersite_config.php");
 
-include_once 'header.php';
-
-if (isset($_SESSION['user']))
-{
-	destroySession();
-	echo "<div class='main'> You have been logged out. Please " .
-	"<a href='index.php'>click here</a> to refesh the screen.";
-}
-
-else echo "<div class='main'><br />" .
-		"You cannot log out because you are not logged in";
+$fgmembersite->LogOut();
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Login</title>
+      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<body>
 
-<br /><br /></div></body></html>
+<h2>You have logged out</h2>
+<p>
+<a href='login.php'>Login Again</a>
+<br/>
+<a href='index.php'>Home</a>
+</p>
+
+</body>
+</html>
