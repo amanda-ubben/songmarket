1c4fd208061e5016fee271a165456c06c1b1c3e9
Matt Ao <mattyayoh@gmail.com>
2014-03-31 17:45:25 -0400

    Fixed up e-mails

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ if(isset($_POST['submitted']))
     <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
     <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
     <link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
-    <script src="scripts/pwdwidget.js" type="text/javascript"></script>      
+    <script src="scripts/pwdwidget.js" type="text/javascript"></script>
 </head>
 <body>
 
@@ -51,10 +51,10 @@ if(isset($_POST['submitted']))
 </div>
 <div class='container' style='height:80px;'>
     <label for='password' >Password*:</label><br/>
-    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
-    <noscript>
+    <!-- <div class='pwdwidgetdiv' id='thepwddiv' ></div> -->
+    <!-- <noscript> -->
     <input type='password' name='password' id='password' maxlength="50" />
-    </noscript>    
+    <!-- </noscript>     -->
     <div id='register_password_errorloc' class='error' style='clear:both'></div>
 </div>
 
@@ -69,9 +69,10 @@ Uses the excellent form validation script from JavaScript-coder.com-->
 
 <script type='text/javascript'>
 // <![CDATA[
-    var pwdwidget = new PasswordWidget('thepwddiv','password');
-    pwdwidget.MakePWDWidget();
-    
+
+    // var pwdwidget = new PasswordWidget('thepwddiv','password');
+    // pwdwidget.MakePWDWidget();
+
     var frmvalidator  = new Validator("register");
     frmvalidator.EnableOnPageErrorDisplay();
     frmvalidator.EnableMsgsTogether();
@@ -82,7 +83,6 @@ Uses the excellent form validation script from JavaScript-coder.com-->
     frmvalidator.addValidation("email","email","Please provide a valid email address");
 
     frmvalidator.addValidation("username","req","Please provide a username");
-    
     frmvalidator.addValidation("password","req","Please provide a password");
 
 // ]]>
